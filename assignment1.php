93bc8a04419f9402c03f67040f4f7d0a88e396f6
MinHeinKhant <0minheinkhant0@gmail.com>
2020-10-13 18:33:42 +0630

    developed by Min Hein Khant

--- assignment1.php
+++ assignment1.php
@@ -11,7 +11,7 @@ $cola       = 85;
 
 $total =$hamburger1+$hamburger2+$milkshake+$cola;
 
-echo "Total is  "."$total"."."."<br><br>";
+echo "Total is  "."$total"."$."."<br><br>";
 
 
 
@@ -19,7 +19,7 @@ $saletax = 0.075;
 
 $totalsaletax = $total * $saletax;
 
-echo "Totalsaletax is  "."$totalsaletax"."."."<br><br>";
+echo "Totaltaxrate is  "."$totalsaletax"."$ ."."<br><br>";
 
 
 $pretax = 0.16;
@@ -30,14 +30,71 @@ $totalpretax = $total*$pretax;
 
 $taxtotal = $saletax+$totalpretax;
 
-echo "Taxtotal is  "."$taxtotal"."."."<br><br>";
+echo "Pre-tax Tip is  "."$taxtotal"."$ ."."<br><br>";
 
 
 $totaltaxandtip = $totalsaletax+$taxtotal;
-echo"Totaltaxandtip is  "."$totaltaxandtip".".";
+echo"Totaltaxandtip is  "."$totaltaxandtip"."$ .<br><br>";
 
 
 
+echo "<table border=1 cellpadding= 10 cellspacing=0>";
 
+echo "
+<tr>
+	<th colspan=4>
+	Slip
+	</th>
+</tr>";
+
+echo "
+<tr>
+	<th>Name</th>
+	<th>Price</th>
+	<th>Qty</th>
+	<th>Total Price</th>
+</tr>";
+echo "
+<tr>
+	<td>Hambagar</td>
+	<td>4.95$</td>
+	<td>2</td>
+	<td>9.9$</td>
+</tr>";
+echo "
+<tr>
+	<td>Chocolate MilkShake</td>
+	<td>1.95$</td>
+	<td>1</td>
+	<td>1.95$</td>
+</tr>";
+echo "
+<tr>
+	<td>Cocacola </td>
+	<td>85$</td>
+	<td>1</td>
+	<td>85$</td>
+</tr>";
+echo "
+<tr>
+	<td colspan=3>Total for all menu</td>
+	<td>96.85$</td>
+</tr>";
+echo "
+<tr>
+	<td colspan=3>Sale Tax Rate</td>
+	<td>7.26375$</td>
+</tr>";
+echo "
+<tr>
+	<td colspan=3>Pre-tax Tip</td>
+	<td>15.496$</td>
+</tr>";
+echo "
+<tr>
+	<td colspan=3>Total tax and tip </td>
+	<td>22.75975$</td>
+</tr>";
+echo "</table>";
 
  ?>
--- chess.php
+++ chess.php
@@ -40,7 +40,17 @@
 	}
 
 	
-
+		echo "<table border=1 cellpadding= 10>";
+		echo "<tr><td colspan=4><center><b>Slip</b></center></td></tr>";
+		echo "<tr><td>Name</td><td>Price</td><td>Qty</td><td>Total Price</td></tr>";
+		echo "<tr><td>Hambagar</td><td>$$hambagar</td><td>2</td><td>$$twoham</td></tr>";
+		echo "<tr><td>Chocolate MilkShake</td><td>$$chocolatemilkshake</td><td>1</td><td>$$chocolatemilkshake</td></tr>";
+		echo "<tr><td>Cocacola </td><td>$$cola</td><td>1</td><td>$$cola</td></tr>";
+		echo "<tr><td colspan=3>Total for all menu</td><td>$$totalmenu</td></tr>";
+		echo "<tr><td colspan=3>Sale Tax Rate</td><td>$$tax</td></tr>";
+		echo "<tr><td colspan=3>Pre-tax Tip</td><td>$$tip</td></tr>";
+		echo "<tr><td colspan=3>Total tax and tip </td><td>$$totalall</td></tr>";
+		echo "</table>";
 
 
 
